b198f05bdab7532c4a54ebf1f039ddbeb8a88285
Ryma8905 <156120862+Ryma8905@users.noreply.github.com>
2024-02-10 22:08:04 +0000

    test

--- Streamlit LFB/Presentation.py
+++ Streamlit LFB/Presentation.py
@@ -1,32 +1,22 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
 from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
 
-
 def run():
     st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+        page_title="London Fire Brigade Project",
+        page_icon="🎆",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    st.write("# London Fire Brigade Project")
 
-    st.write("titit ")
+    st.write("La brigade des pompiers de Londres, comme mentionné dans la fiche projet est l’une des plus grandes au monde.")
+    st.write("Dans le cadre de notre projet de recherche, nous devrons estimer et analyser les temps de réponse et d’intervention")
+    st.write("de la brigade.") 
+    st.write("Les sources de données utilisées pour répondre à cette problématique proviennent du site ‘London Datastore’")
+    st.write("et sont rafraichies sur une base mensuelle. Elles fournissent le détail des incidents traités depuis janvier 2009.") 
+
 
     st.sidebar.success("Select a demo above.")
 
