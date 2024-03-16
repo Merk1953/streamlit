9d78a87aa9e6d08e0109d2289a479ac5ef2c7fd3
Ryma8905 <156120862+Ryma8905@users.noreply.github.com>
2024-03-16 11:49:34 +0000

    update

--- pages/3_📈_Modélisation.py
+++ pages/3_📈_Modélisation.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import joblib
 import base64
+from sklearn.preprocessing import StandardScaler
 
 # Créer les onglets
 with st.sidebar:
@@ -145,64 +146,53 @@ elif onglet == "Modèles étudiés":
 
 elif onglet == "Démo":
     
-    # Charger le modèle entraîné
-    regressor_lin = joblib.load('Model/regressor_lin.joblib')  
+
+    
+    # Chargement du modèle
+    regressor_lin = joblib.load('Model/regressor_lin.joblib')
 
     # Variables ProperCase et StopCodeDescription et PropertyCategory
     proper_case_values = ["Barnet", "Bromley", "Camden", "Enfield", "Hillingdon", "Kensington And Chelsea", "Lambeth", "Southwark", "Tower Hamlets"]
     stop_code_description_values = ["AFA", "False alarm - Good intent"]
     property_category_values = ["Non Residential", "Other Residential", "Outdoor", "Road Vehicle"]
 
-    # Mois, année, jour de la semaine et heure de la journée
-    #months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    #years = [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023]  
-    #days_of_week = [1, 2, 3, 4, 5, 6, 7]
-    #hours_of_day = list(range(24))
-
-    # Interface utilisateur avec Streamlit
+    # Variables explicatives de lieu et type de propriété
     st.title("Prédiction de la variable cible avec un modèle de régression linéaire")
     st.write("### Entrez les valeurs des variables explicatives :")
 
-    # Variables explicatives temporelles 
-    #st.write("Variables explicatives temporelles :")
-    #jour_sem_num = st.selectbox("Jour de la semaine (Numéro 1-7)", options=days_of_week)
-    #mois = st.selectbox("Mois", options=months)
-    #année = st.selectbox("Année", options=years)
-    
-    #hour_of_call = st.selectbox("Heure d'appel", options=hours_of_day)
-
-    # Variables ProperCase et StopCodeDescription et property category
-    st.write("**Variables explicatives de lieu :**")
     proper_case_selected = st.selectbox("Choisissez un qartier de survenance de l'incident", options=proper_case_values)
     stop_code_description_selected = st.selectbox("Choisissez un type d'incident", options=stop_code_description_values)
-
-    st.write("**Variable explicative de type de propriété :**")
     property_category_selected = st.selectbox("Choisissez un type de propriété", options=property_category_values)
-   
-    # Encodage des variables ProperCase, Mois, Année, Jour de la semaine et Heure de la journée
+
+    # Encodage des variables ProperCase, StopCodeDescription et PropertyCategory
     proper_case_encoded = [1 if proper_case == proper_case_selected else 0 for proper_case in proper_case_values]
-    #mois_encoded = [1 if month == mois else 0 for month in months]
-    #année_encoded = [1 if year == année else 0 for year in years]
-    #jour_sem_num_encoded = [1 if day == jour_sem_num else 0 for day in days_of_week]
-    #hour_of_call_encoded = [1 if hour == hour_of_call else 0 for hour in hours_of_day]
-    property_category_encoded = [1 if category == property_category_selected else 0 for category in property_category_values]
     stop_code_description_encoded = [1 if code == stop_code_description_selected else 0 for code in stop_code_description_values]
+    property_category_encoded = [1 if category == property_category_selected else 0 for category in property_category_values]
 
-    # Autres variables explicatives 
+    # Autres variables explicatives
     st.write("**Autres variables explicatives :**")
-    num_stations = st.number_input(label="Nombre de stations avec pompes intervenantes", min_value=1, max_value=10, step=1, format="%d")
-    pump_count = st.number_input(label="Nombre de pompes utilisées", min_value=1, step=1, format="%d")
-    pump_hours = st.number_input(label="Nombre d'heures d'intervention (arrondies)", min_value=1, step=1, format="%d")
-    turnout_time = st.number_input(label="Temps d'intervention (en secondes)", min_value=60, step=1, format="%d")
-    pump_order = st.number_input(label="Numéro de la pompe", min_value=1, step=1, format="%d")
 
-    # Créer un tableau NumPy avec les valeurs saisies par l'utilisateur
-    user_input = np.array([[*proper_case_encoded,
-                            *property_category_encoded, *stop_code_description_encoded, num_stations, pump_count, pump_hours,
-                            turnout_time, pump_order]])
+    # Collecte des variables
+    num_stations = st.number_input(label="Nombre de stations avec pompes intervenantes", min_value=1, max_value=10, step=1, format="%d", key="num_stations")
+    pump_count = st.number_input(label="Nombre de pompes utilisées", min_value=1, step=1, format="%d", key="pump_count")
+    pump_hours = st.number_input(label="Nombre d'heures d'intervention (arrondies)", min_value=1, step=1, format="%d", key="pump_hours")
+    notional_cost = st.number_input(label="Coût notionnel (£)", min_value=0.0, step=1.0, format="%.2f", key="notional_cost")
+    turnout_time = st.number_input(label="Temps d'intervention (en secondes)", min_value=60, step=1, format="%d", key="turnout_time")
+
+    # Mettre les variables dans un tableau NumPy
+    user_input = np.array([[*proper_case_encoded, *property_category_encoded, *stop_code_description_encoded]])
+
+    # Standardisation des variables numériques
+    scaler = StandardScaler()
+    variables_a_standardiser = np.array([[num_stations, pump_count, pump_hours, notional_cost, turnout_time]])
+    user_input_scaled = np.concatenate((user_input, scaler.fit_transform(variables_a_standardiser)), axis=1)
 
     # Prédiction avec le modèle chargé
-    prediction = regressor_lin.predict(user_input)
+    prediction = regressor_lin.predict(user_input_scaled)
+
+    # Affichage de la prédiction
+    #st.write("**Prédiction de la variable cible :**", prediction[0])
+
 
     # Affichage
     st.write("**Prédiction de la variable cible :**", prediction[0][0])
--- pages/4_🧱_Interprétabilité.py
+++ pages/4_🧱_Interprétabilité.py
@@ -6,14 +6,7 @@ st.title("Interprétabilité")
 st.write("""Maintenant que le modèle de régression linéaire a été sélectionné, il convient de vérifier dans quelle mesure 
          chaque caractéristique contribue au modèle de manière équitable en utilisant SHAP pour améliorer l’interprétabilité. 
          """)
-st.write("""Ensuite, nous passerons au PDP pour voir comment la variation d’une caractéristique particulière influence le 
-         modèle tout en gardant les autres variables constantes afin de pouvoir visualiser les relations entre une variable 
-         spécifique et la sortie du modèle. """)
-st.write("""En effet, l'analyse des graphiques PDP constitue une composante essentielle dans l'interprétation des modèles prédictifs, 
-         en particulier dans le domaine de l'apprentissage automatique. Ces graphiques fournissent des insights significatifs 
-         sur la relation entre les variables d'entrée et la sortie du modèle, permettant ainsi de mieux comprendre 
-         le comportement global du modèle.
- """)
+
 
 #st.sidebar.header("Interprétabilité")
 #choix_inter = st.sidebar.selectbox("", ["Shap", "PDP"])
